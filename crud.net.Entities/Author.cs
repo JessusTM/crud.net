8528a0994e8110e579ecde8c2af9426b46ffc209
JessusTM <jesustapiamartin@gmail.com>
2025-01-06 19:11:33 -0300

    feat(Entities): agregar validaciones a la clase 'Author'
    - Se agregaron validaciones a las propiedades de la clase Author para asegurar que los campos obligatorios sean proporcionados.
    	- Validaciones de requerimiento para las propiedades Name y Books.

---
+++ crud.net.Entities/Author.cs
@@ -0,0 +1,11 @@
+﻿using crud.net.Entities;
+
+namespace api_crud_net.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required List<Book> Books { get; set; }
+    }
+}

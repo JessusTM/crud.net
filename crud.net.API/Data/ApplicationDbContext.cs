08b2919b02c27fe8adc16e01c5a8a8c27f07900b
JessusTM <jesustapiamartin@gmail.com>
2025-01-06 20:54:30 -0300

    feat(Database): configurar relaciones muchos a muchos en ApplicationDbContext
    - Se configuraron las relaciones muchos a muchos en el archivo ApplicationDbContext.cs para las entidades Book, Genre y Author.
    - Definición de las propiedades DbSet para Book, Genre y Author.
    - Configuración de las relaciones muchos a muchos entre Book y Genre y entre Book y Author en el método OnModelCreating.
    
    Este archivo es útil para definir el contexto de la base de datos y las relaciones entre las entidades en el proyecto.

---
+++ crud.net.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crud.net.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasMany(b => b.Genres)
+                .WithMany(g => g.Books);
+
+            modelBuilder.Entity<Book>()
+                .HasMany(b => b.Authors)
+                .WithMany(a => a.Books);
+        }
+    }
+}

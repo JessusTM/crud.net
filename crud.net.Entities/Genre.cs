e484ad9ade2a31ebffd137e3b95c39f720f6492d
JessusTM <jesustapiamartin@gmail.com>
2025-01-06 19:13:41 -0300

    feat(Entities): agregar validaciones a la clase 'Genre'
    - Se agregaron validaciones a las propiedades de la clase 'Genre' para asegurar que los campos obligatorios sean proporcionados.
    	- Validaciones de requerimiento para las propiedades Name y Books.

---
+++ crud.net.Entities/Genre.cs
@@ -0,0 +1,9 @@
+﻿namespace crud.net.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required List<Book> Books { get; set; }
+    }
+}

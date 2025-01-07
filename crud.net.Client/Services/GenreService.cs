195785a4ef5c0336bed46806b8d54217f68afae1
JessusTM <jesustapiamartin@gmail.com>
2025-01-07 03:57:21 -0300

    feat(Service): implementar servicio de géneros en el cliente
    - Se añadió la clase 'GenreService' en el proyecto cliente para interactuar con la API de géneros.
    - Se agregó el método 'GetGenres' que obtiene una lista de géneros desde la API utilizando 'HttpClient'.
    - Se maneja la excepción en el método 'GetGenres' para retornar una lista vacía en caso de fallo.

---
+++ crud.net.Client/Services/GenreService.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Json;
+using crud.net.Entities.Domain;
+
+namespace crud.net.Client.Services
+{
+    public class GenreService
+    {
+        private readonly HttpClient _httpClient;
+
+        public GenreService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Genre>> GetGenres()
+        {
+            try
+            {
+                var response = await _httpClient.GetFromJsonAsync<List<Genre>>("api/genre");
+                return response ?? new List<Genre>();
+            }
+            catch (Exception)
+            {
+                return new List<Genre>();
+            }
+        }
+    }
+}

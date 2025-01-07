4c0b85db49ddff3b44b05daa038664b544812a9a
JessusTM <jesustapiamartin@gmail.com>
2025-01-07 03:27:10 -0300

    feat(Repository): implementar repositorio de géneros
    - Se añadió la clase 'GenreRepository' que implementa la interfaz 'IGenreRepository'.
    - Se agregó el método 'GetGenres' que obtiene una lista de géneros desde la base de datos utilizando Entity Framework Core.
    - Se inyecta 'ApplicationDbContext' a través del constructor para acceder al contexto de la base de datos.

---
+++ crud.net.API/Repositories/GenreRepository.cs
@@ -0,0 +1,22 @@
+﻿using crud.net.API.Data;
+using crud.net.Entities.Domain;
+using crud.net.Entities.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.net.API.Repositories
+{
+    public class GenreRepository : IGenreRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenreRepository(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<List<Genre>> GetGenres()
+        {
+            return await _context.Genres.ToListAsync();
+        }
+    }
+}

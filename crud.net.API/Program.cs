a61dc258b9c1c0256d406bc6b961e85127c2a989
JessusTM <jesustapiamartin@gmail.com>
2025-01-07 00:10:37 -0300

    feat(Program): configurar servicios y opciones de serialización en 'Program.cs'
    - Añadido el uso de System.Text.Json.Serialization para manejar ciclos de referencia.
    - Registrados los servicios BookService y BookRepository en el contenedor de dependencias.
    - Configuradas las opciones de serialización JSON para ignorar ciclos de referencia.
    
    Estos cambios aseguran que los servicios necesarios estén disponibles para la inyección de dependencias y que la serialización JSON maneje correctamente los ciclos de referencia, evitando errores y mejorando la estabilidad de la aplicación.
    
    Detalles técnicos:
    	- `builder.Services.AddScoped<BookService>()`: Registra BookService con una vida útil de Scoped, creando una nueva instancia por cada solicitud HTTP.
    	- `builder.Services.AddScoped<IBookRepository, BookRepository>()`: Registra BookRepository como la implementación de IBookRepository con una vida útil de Scoped.
      	- `builder.Services.AddControllers().AddJsonOptions(options => { options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles; })`: Configura las opciones de serialización JSON para ignorar ciclos de referencia, evitando errores de serialización cuando hay referencias cíclicas en los objetos.

--- crud.net.API/Program.cs
+++ crud.net.API/Program.cs
@@ -1,15 +1,25 @@
+using System.Text.Json.Serialization;
 using crud.net.API.Data;
+using crud.net.API.Repositories;
+using crud.net.API.Services;
+using crud.net.Entities.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<BookService>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddControllers()
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())

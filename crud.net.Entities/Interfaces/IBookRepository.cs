c222048646398a627f5c4c5d1b51f4b515f8da1b
JessusTM <jesustapiamartin@gmail.com>
2025-01-06 22:00:54 -0300

    feat(Entities): agregar interfaz 'IBookRepository'
    - Se agregó la interfaz 'IBookRepository' en el proyecto 'Entities' para definir las operaciones CRUD para la entidad Book.
    	- Definición de métodos para obtener todos los libros, obtener un libro por ID, agregar un libro, actualizar un libro y eliminar un libro.
    
    Esta interfaz es útil para abstraer las operaciones de acceso a datos de la entidad Book y facilitar la implementación de repositorios.

---
+++ crud.net.Entities/Interfaces/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using crud.net.Entities.Domain;
+
+namespace crud.net.Entities.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task<List<Book>> GetBooks();
+        Task<Book> GetBookById(int id);
+        Task<Book> AddBook(Book book);
+        Task<Book> UpdateBook(int id, Book book);
+        Task DeleteBook(int id);
+    }
+}

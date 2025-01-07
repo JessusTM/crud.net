f183378c9d1c4c2baa562ede77e6777a793aa27c
JessusTM <jesustapiamartin@gmail.com>
2025-01-07 01:58:39 -0300

    feat(Service): agregar servicio de libros para obtener datos desde la API
    - Se añadió el servicio 'BookService' para manejar la obtención de datos de libros.
    - Se implementó el método 'GetBooks' para obtener la lista de libros desde la API.
    - Se manejaron excepciones para retornar una lista vacía en caso de error.

---
+++ crud.net.Client/Services/BookService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Json;
+using crud.net.Entities.Domain;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace crud.net.Client.Services
+{
+    public class BookService
+    {
+        private List<Book> books;
+        private readonly HttpClient _httpClient;
+
+        public BookService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            books = new List<Book>();
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            try
+            {
+                var response = await _httpClient.GetFromJsonAsync<List<Book>>("api/book");
+                return response ?? new List<Book>();
+            }
+            catch (Exception)
+            {
+                return new List<Book>();
+            }
+        }
+    }
+}

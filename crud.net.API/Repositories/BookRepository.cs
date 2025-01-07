7187d0371c547fbfec282d565b3d1274a1f49c3d
JessusTM <jesustapiamartin@gmail.com>
2025-01-06 22:18:57 -0300

    feat(Repository): implementar clase repository para manejar operaciones de datos
    - Se creó la clase 'BookRepository' que implementa la interfaz 'IBookRepository' y gestiona las operaciones CRUD sobre la entidad 'Book'.
    - Se añadieron validaciones mínimas para garantizar la integridad de datos, como excepciones 'KeyNotFoundException' en los métodos 'GetBookById', 'DeleteBook' y 'UpdateBook'.
    - Se utilizó 'Entity Framework Core' para interactuar con la base de datos mediante 'ApplicationDbContext'.
    - Este commit sienta las bases para una separación clara de responsabilidades entre el Repository, el Service y el Controller.

---
+++ crud.net.API/Repositories/BookRepository.cs
@@ -0,0 +1,55 @@
+﻿using crud.net.API.Data;
+using crud.net.Entities.Domain;
+using crud.net.Entities.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.net.API.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Book> AddBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null) throw new KeyNotFoundException($"Book with id {id} not found.");
+            
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Book> GetBookById(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null) throw new KeyNotFoundException($"Book with id {id} not found.");
+            return book;
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        public async Task<Book> UpdateBook(int id, Book book)
+        {
+            var foundBook = await _context.Books.FindAsync(id);
+            if (foundBook == null) throw new KeyNotFoundException($"Book with id {id} not found.");
+            
+            _context.Entry(foundBook).CurrentValues.SetValues(book);
+            await _context.SaveChangesAsync();
+            return foundBook;
+        }
+    }
+}

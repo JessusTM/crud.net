dd95c3a1f01d0452323fb6b3225cd5c67c0a397f
JessusTM <jesustapiamartin@gmail.com>
2025-01-07 01:24:37 -0300

    feat(API): configurar CORS para permitir la conexión del proyecto Blazor con la API
    - Añadida configuración de CORS para permitir cualquier origen, método y encabezado.
    - Configuradas opciones de serialización JSON para ignorar ciclos de referencia.
    - Registrados los servicios BookService y BookRepository en el contenedor de dependencias.
    - Configurada la política de CORS en el middleware de la aplicación.
    
    Estos cambios permiten que el proyecto Blazor se conecte correctamente con la API, asegurando que las solicitudes HTTP se manejen adecuadamente y que los datos se serialicen sin problemas de ciclos de referencia.

--- crud.net.API/Program.cs
+++ crud.net.API/Program.cs
@@ -7,19 +7,26 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddOpenApi();
 builder.Services.AddScoped<BookService>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddControllers()
-.AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -27,10 +34,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();

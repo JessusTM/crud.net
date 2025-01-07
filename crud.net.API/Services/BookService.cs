00dac32b77015a769bff225e3fa4e61fd3c730c0
JessusTM <jesustapiamartin@gmail.com>
2025-01-07 00:03:50 -0300

    refactor(Services): Actualizar mensajes de excepción en 'BookService'
    - Se modifican  los mensajes de excepción en los métodos 'GetBookById', 'AddBook', 'UpdateBook' y 'DeleteBook' para incluir el contexto [BookService].
    - Los mensajes ahora son más claros y específicos sobre el origen del error.

--- crud.net.API/Services/BookService.cs
+++ crud.net.API/Services/BookService.cs
@@ -19,26 +19,26 @@ namespace crud.net.API.Services
 
         public async Task<Book> GetBookById(int id)
         {
-            if (id <= 0) throw new ArgumentException("El ID debe ser un valor positivo.");
+            if (id <= 0) throw new ArgumentException("[BookService] ID must be a positive value.");
             return await _bookRepository.GetBookById(id);
         }
 
         public async Task<Book> AddBook(Book book)
         {
-            if (book.Stock < 0) throw new ArgumentException("El stock no puede ser negativo.");
+            if (book.Stock < 0) throw new ArgumentException("[BookService] Stock cannot be negative.");
             return await _bookRepository.AddBook(book);
         }
 
         public async Task<Book> UpdateBook(int id, Book book)
         {
-            if (id <= 0) throw new ArgumentException("El ID debe ser un valor positivo.");
-            if (book.Stock < 0) throw new ArgumentException("El stock no puede ser negativo.");
+            if (id <= 0) throw new ArgumentException("[BookService] ID must be a positive value.");
+            if (book.Stock < 0) throw new ArgumentException("[BookService] Stock cannot be negative.");
             return await _bookRepository.UpdateBook(id, book);
         }
 
         public async Task DeleteBook(int id)
         {
-            if (id <= 0) throw new ArgumentException("El ID debe ser un valor positivo.");
+            if (id <= 0) throw new ArgumentException("[BookService] ID must be a positive value.");
             await _bookRepository.DeleteBook(id);
         }
     }

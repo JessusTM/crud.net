bec47d728301fc9473f99560da8ffada3d4bfb6c
JessusTM <jesustapiamartin@gmail.com>
2025-01-06 22:35:22 -0300

    feat(Controller): agregar clase controller para manejar las solicitudes HTTP relacionadas con los libros
    - Se agregó la clase 'BookController' para manejar las solicitudes HTTP relacionadas con los libros.
    	- Métodos para obtener todos los libros, obtener un libro por ID, agregar un libro, actualizar un libro y eliminar un libro.
    	- Manejo de excepciones para devolver códigos de estado HTTP apropiados en caso de errores.
    
    Esta clase es útil para exponer la funcionalidad de la API y permitir la interacción con los servicios de libros a través de solicitudes HTTP.

---
+++ crud.net.API/Controllers/BookController.cs
@@ -0,0 +1,89 @@
+﻿using crud.net.API.Services;
+using crud.net.Entities.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud.net.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BookController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBooks()
+        {
+            try
+            {
+                var response = await _bookService.GetBooks();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error retrieving data from the database: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBookById(int id)
+        {
+            try
+            {
+                var response = await _bookService.GetBookById(id);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error retrieving data from the database: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> AddBook(Book book)
+        {
+            try
+            {
+                var response = await _bookService.AddBook(book);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error adding data to the database: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Book>> UpdateBook(int id, Book book)
+        {
+            try
+            {
+                var response = await _bookService.UpdateBook(id, book);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error updating data in the database: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DeleteBook(int id)
+        {
+            try
+            {
+                await _bookService.DeleteBook(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error deleting data from the database: {ex.Message}");
+            }
+        }
+    }
+}

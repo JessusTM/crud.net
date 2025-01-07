820c688127d71deb9d87fb77c0d25761b4604c4b
JessusTM <jesustapiamartin@gmail.com>
2025-01-07 03:29:19 -0300

    feat(Controller): implementar controlador de géneros
    - Se añadió la clase 'GenreController' que expone endpoints para la gestión de géneros.
    - Se agregó el método 'GetGenres' que obtiene una lista de géneros a través del servicio de géneros.
    - Se inyecta 'GenreService' a través del constructor para acceder a los métodos del servicio.
    - Se maneja la excepción en el método 'GetGenres' para retornar un error 500 en caso de fallo.

---
+++ crud.net.API/Controllers/GenreController.cs
@@ -0,0 +1,32 @@
+﻿using crud.net.API.Services;
+using crud.net.Entities.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud.net.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private readonly GenreService _genreService;
+
+        public GenreController(GenreService genreService)
+        {
+            _genreService = genreService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Genre>>> GetGenres()
+        {
+            try
+            {
+                var response = await _genreService.GetGenres();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"[GenreController] Error retrieving data from the database: {ex.Message} | Inner: {ex.InnerException?.Message}");
+            }
+        }
+    }
+}

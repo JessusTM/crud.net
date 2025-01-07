f71bd95242fba8e6e92e9ee208e8748ced9b2814
JessusTM <jesustapiamartin@gmail.com>
2025-01-06 22:29:23 -0300

    feat(Services): agregar clase service para lógica de negocio de los libros
    - Se agregó la clase 'BookService' para manejar la lógica de negocio relacionada con los libros.
    	- Métodos para obtener todos los libros, obtener un libro por ID, agregar un libro, actualizar un libro y eliminar un libro.
      	- Validaciones para asegurar que el ID sea un valor positivo y que el stock no sea negativo.
    
    Esta clase es útil para separar la lógica de negocio de la lógica de acceso a datos, mejorando la organización y mantenibilidad del código.

---
+++ crud.net.API/Services/BookService.cs
@@ -0,0 +1,45 @@
+﻿using crud.net.Entities.Domain;
+using crud.net.Entities.Interfaces;
+
+namespace crud.net.API.Services
+{
+    public class BookService
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            return await _bookRepository.GetBooks();
+        }
+
+        public async Task<Book> GetBookById(int id)
+        {
+            if (id <= 0) throw new ArgumentException("El ID debe ser un valor positivo.");
+            return await _bookRepository.GetBookById(id);
+        }
+
+        public async Task<Book> AddBook(Book book)
+        {
+            if (book.Stock < 0) throw new ArgumentException("El stock no puede ser negativo.");
+            return await _bookRepository.AddBook(book);
+        }
+
+        public async Task<Book> UpdateBook(int id, Book book)
+        {
+            if (id <= 0) throw new ArgumentException("El ID debe ser un valor positivo.");
+            if (book.Stock < 0) throw new ArgumentException("El stock no puede ser negativo.");
+            return await _bookRepository.UpdateBook(id, book);
+        }
+
+        public async Task DeleteBook(int id)
+        {
+            if (id <= 0) throw new ArgumentException("El ID debe ser un valor positivo.");
+            await _bookRepository.DeleteBook(id);
+        }
+    }
+}

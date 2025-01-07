b1faef6bf69cd005ca31605a339424fa0a827866
JessusTM <jesustapiamartin@gmail.com>
2025-01-07 02:05:19 -0300

    chore(Frontend): configurar BookService y HttpClient en Program.cs
    - Se añadió el espacio de nombres 'crud.net.Client.Services' a Program.cs.
    - Se actualizó la dirección base de HttpClient a 'http://localhost:5244'.
    - Se registró 'BookService' como un servicio de ámbito en el contenedor de dependencias.

--- crud.net.Client/Program.cs
+++ crud.net.Client/Program.cs
@@ -1,4 +1,5 @@
 using crud.net.Client;
+using crud.net.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5244") });
+builder.Services.AddScoped<BookService>();
 
 await builder.Build().RunAsync();

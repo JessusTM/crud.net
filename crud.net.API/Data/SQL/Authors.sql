c537f98fdb273edcebf30b40ca5b2efc47d6e5cb
JessusTM <jesustapiamartin@gmail.com>
2025-01-07 00:09:07 -0300

    feat(Database): agregar script SQL para insertar autores en la base de datos
    - Añadido script SQL para insertar registros en la tabla Authors.
    - El script incluye la inserción de 20 autores con sus respectivos IDs y nombres.
    - Activado y desactivado IDENTITY_INSERT para permitir la inserción de IDs específicos.

--- crud.net.API/Data/SQL/Authors.sql
+++ crud.net.API/Data/SQL/Authors.sql
@@ -1,6 +1,26 @@
-﻿namespace crud.net.API.Data
-{
-    public class Scripts
-    {
-    }
-}
+﻿SET IDENTITY_INSERT [db_crud_net].[dbo].[Authors] ON;
+
+INSERT INTO [db_crud_net].[dbo].[Authors] ([Id], [Name])
+VALUES 
+(1, 'Albert Camus'),
+(2, 'Fyodor Dostoevsky'),
+(3, 'George Orwell'),
+(4, 'Ernest Hemingway'),
+(5, 'Franz Kafka'),
+(6, 'Jane Austen'),
+(7, 'Leo Tolstoy'),
+(8, 'Marcel Proust'),
+(9, 'Virginia Woolf'),
+(10, 'Herman Melville'),
+(11, 'William Faulkner'),
+(12, 'John Steinbeck'),
+(13, 'Charles Dickens'),
+(14, 'Gabriel García Márquez'),
+(15, 'Mark Twain'),
+(16, 'Oscar Wilde'),
+(17, 'Jack London'),
+(18, 'Henry James'),
+(19, 'F. Scott Fitzgerald'),
+(20, 'T.S. Eliot');
+
+SET IDENTITY_INSERT [db_crud_net].[dbo].[Authors] OFF;

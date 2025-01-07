acc876ecf09fceec54bd010ed62c61da100246fe
JessusTM <jesustapiamartin@gmail.com>
2025-01-07 03:28:25 -0300

    feat(Service): implementar servicio de géneros
    - Se añadió la clase 'GenreService' que utiliza el repositorio de géneros.
    - Se agregó el método 'GetGenres' que obtiene una lista de géneros a través del repositorio.
    - Se inyecta 'GenreRepository' a través del constructor para acceder a los métodos del repositorio.

---
+++ crud.net.API/Services/GenreService.cs
@@ -0,0 +1,20 @@
+﻿using crud.net.API.Repositories;
+using crud.net.Entities.Domain;
+
+namespace crud.net.API.Services
+{
+    public class GenreService
+    {
+        private readonly GenreRepository _genreRepository;
+
+        public GenreService(GenreRepository genreRepository)
+        {
+            _genreRepository = genreRepository;
+        }
+
+        public async Task<List<Genre>> GetGenres()
+        {
+            return await _genreRepository.GetGenres();
+        }
+    }
+}

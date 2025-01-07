907489a85293756dd0cdc4f6918b6e86bbf2c87f
JessusTM <jesustapiamartin@gmail.com>
2025-01-07 00:14:28 -0300

    feat(Database): Añadir script SQL para eliminar todos los datos de las tablas en DeleteAll.sql
    - Añadido script SQL para desactivar temporalmente las restricciones de clave foránea.
    - Añadido script SQL para borrar los datos de las tablas BookGenre, Books, Authors y Genres respetando la relación entre ellas.
    - Añadido script SQL para reactivar las restricciones de clave foránea.

---
+++ crud.net.API/Data/SQL/DeleteAll.sql
@@ -0,0 +1,11 @@
+﻿-- Desactivar temporalmente las restricciones de clave foránea
+ALTER TABLE [db_crud_net].[dbo].[BookGenre] NOCHECK CONSTRAINT ALL;
+
+-- Borrar los datos de las tablas respetando la relación entre ellas
+DELETE FROM [db_crud_net].[dbo].[BookGenre];
+DELETE FROM [db_crud_net].[dbo].[Books];
+DELETE FROM [db_crud_net].[dbo].[Authors];
+DELETE FROM [db_crud_net].[dbo].[Genres];
+
+-- Reactivar las restricciones de clave foránea
+ALTER TABLE [db_crud_net].[dbo].[BookGenre] CHECK CONSTRAINT ALL;
